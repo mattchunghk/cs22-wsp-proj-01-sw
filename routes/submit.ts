71db5e4abdb573762a74cb25d0d30d081f1ce1cd
mattchunghk <mattchunghk@gmail.com>
2022-09-10 20:44:48 +0800

    utils

---
+++ routes/submit.ts
@@ -0,0 +1,6 @@
+import express, { Request, Response } from "express";
+import { client } from "../utils/db";
+import { checkPassword, hashPassword } from "../utils/hash";
+import fetch from "cross-fetch";
+import crypto from "crypto";
+import { logger } from "../utils/logger";
