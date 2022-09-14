c8955f135c0c717e0d0eabccad646149899a16ea
mattchunghk <mattchunghk@gmail.com>
2022-09-14 17:42:23 +0800

    session Done

--- routes/userRoute.ts
+++ routes/userRoute.ts
@@ -43,7 +43,6 @@ userRoutes.post("/login", async (req, res) => {
   console.log("userRoutes - [/login]");
   const username = req.body.username;
   const password = req.body.password;
-  console.log(username, password);
 
   if (!username || !password) {
     res.status(400).json({
@@ -79,7 +78,7 @@ userRoutes.post("/login", async (req, res) => {
   req.session["name"] = username;
   req.session["userId"] = dbuser.id;
   req.session["isAdmin"] = dbuser.is_admin;
-  console.log("login");
+  console.log("Login successful");
   res.status(200).json({
     message: "Success login",
   });
