147e6b8b83c17af5c2efe9872d2f2e116896400b
mattchunghk <mattchunghk@gmail.com>
2022-09-19 22:36:17 +0800

    google search

--- eventsForm/eventsForm.js
+++ eventsForm/eventsForm.js
@@ -5,6 +5,7 @@ window.onload = () => {
 	loadCountryFunction()
 	getFunctionBar()
 	loginStatus()
+	initMap()
 }
 
 function init() {
@@ -155,4 +156,25 @@ async function loginStatus() {
 			document.querySelector('#country-selection').disabled = true
 		}
 	}
+}
+
+function initMap() {
+	// const center = { lat: 50.064192, lng: -130.605469 }
+	// // Create a bounding box with sides ~10km away from the center point
+	// const defaultBounds = {
+	// 	north: center.lat + 0.1,
+	// 	south: center.lat - 0.1,
+	// 	east: center.lng + 0.1,
+	// 	west: center.lng - 0.1
+	// }
+	// const country =  document.getElementById('country-selection')
+	const input = document.getElementById('place')
+	const options = {
+		// bounds: defaultBounds,
+		// componentRestrictions: { country: 'us' },
+		fields: ['address_components', 'geometry', 'icon', 'name'],
+		strictBounds: false,
+		types: ['establishment']
+	}
+	const autocomplete = new google.maps.places.Autocomplete(input, options)
 }
--- public/createEvent.js
+++ public/createEvent.js
@@ -1,68 +1,54 @@
 window.onload = () => {
-    loadMessages();
-};
-
-const socket = io.connect();
-socket.on("new-message", (data) => {
-    console.log(data);
-    loadMessages();
-});
-socket.on("new-message-update", (data) => {
-    console.log(data);
-    loadMessages();
-});
-socket.on("message-delete", (data) => {
-    console.log(data);
-    loadMessages();
-});
+	loadMessages()
+}
 
 //寫留言
 
 document
-    .querySelector("#messages-form")
-    .addEventListener("submit", async(event) => {
-        event.preventDefault();
-        console.log("#messages-form");
-        const messageData = new FormData(event.currentTarget);
-        console.log(messageData);
+	.querySelector('#messages-form')
+	.addEventListener('submit', async (event) => {
+		event.preventDefault()
+		console.log('#messages-form')
+		const messageData = new FormData(event.currentTarget)
+		console.log(messageData)
 
-        const res = await fetch("/messages/create", {
-            method: "POST",
-            body: messageData,
-        });
-        const result = await res.text();
-        if (res.status === 200) {
-            console.log(result);
-        }
-    });
+		const res = await fetch('/messages/create', {
+			method: 'POST',
+			body: messageData
+		})
+		const result = await res.text()
+		if (res.status === 200) {
+			console.log(result)
+		}
+	})
 
 //拎messages & images
 async function loadMessages() {
-    const messagesResult = await fetch("/messages/get");
-    const messagesJson = await messagesResult.json();
-    console.log(messagesJson);
+	const messagesResult = await fetch('/messages/get')
+	const messagesJson = await messagesResult.json()
+	console.log(messagesJson)
 
-    // const messagesLikeResult = await fetch('/message_like/get');
-    // const messagesLikeJson = await messagesLikeResult.json();
-    // console.log(messagesLikeJson);
+	// const messagesLikeResult = await fetch('/message_like/get');
+	// const messagesLikeJson = await messagesLikeResult.json();
+	// console.log(messagesLikeJson);
 
-    const messagesContainer = document.querySelector("#messages_container");
-    messagesContainer.innerHTML = "";
-    // ${memo.image ? `<img src="http://localhost:8080/uploads/${memo.image}" alt="Image" /> ` : ""}
+	const messagesContainer = document.querySelector('#messages_container')
+	messagesContainer.innerHTML = ''
+	// ${memo.image ? `<img src="http://localhost:8080/uploads/${memo.image}" alt="Image" /> ` : ""}
 
-    for (let message of messagesJson) {
-        const messageId = message.id;
-        let imageHtml = "";
-        // console.log(message.images);
-        if (message.images != "") {
-            for (let messagesImage of message.images) {
-                // console.log(messagesImage);
-                imageHtml += `
+	for (let message of messagesJson) {
+		const messageId = message.id
+		let imageHtml = ''
+		// console.log(message.images);
+		if (message.images != '') {
+			for (let messagesImage of message.images) {
+				// console.log(messagesImage);
+				imageHtml += `
             <div class="carousel-item active">
                     <img src="http://localhost:8080/${messagesImage}" class=" d-block w-100 " alt="..."> 
-            </div>`;
-            }
-            messagesContainer.innerHTML += `
+            </div>`
+			}
+			messagesContainer.innerHTML += `
         <div class="messageAllDivs" >
             <div class="row heading-setting">
                 <div for="exampleFormControlTextarea1" class="form-label col-md-3" >${message.heading}</div>
@@ -116,20 +102,20 @@ async function loadMessages() {
                     </button>
                   </div>
                  </div>
-                 </div>`;
-        } else {
-            messagesContainer.innerHTML += `
+                 </div>`
+		} else {
+			messagesContainer.innerHTML += `
         <div class="messageAllDivs" >
             <div class="row heading-setting">
                 <div for="exampleFormControlTextarea1" class="form-label col-md-3" >${
-                  message.heading
-                }</div>
+					message.heading
+				}</div>
                 <div class="col-md-3"> Name: <span>  ${
-                  message.user_id
-                }</span></div>
+					message.user_id
+				}</span></div>
                 <div class="col-md-4"> Date: <span> ${
-                  message.created_at
-                }1</span></div>
+					message.created_at
+				}1</span></div>
             </div>
                 <div class="row">
                     <div class="col-md-12 box1">
@@ -138,130 +124,131 @@ async function loadMessages() {
                         <div class="row button-div">
                             <div class="col-md-3">
                                 <button class='delete-btn ' index="${
-                                  message.id
-                                }">
+									message.id
+								}">
                                     <i class="bi bi-trash3-fill" index="${
-                                      message.id
-                                    }"></i>
+										message.id
+									}"></i>
                                 </button>
                             </div>
                             <div class="col-md-3">
                                 <button class='edit-btn' index="${message.id}">
                                     <i class="bi bi-pencil-square" index="${
-                                      message.id
-                                    }"></i>
+										message.id
+									}"></i>
                                 </button>
                             </div>
                             <div class="col-md-3">
                                 <button class='angry-btn' index="${message.id}">
                                     <i class="bi bi-emoji-angry index="${
-                                      message.id
-                                    }"></i>
+										message.id
+									}"></i>
                                     <span class="angryview" id="angry_value" index="${
-                                      message.id
-                                    }">0</span>
+										message.id
+									}">0</span>
                                 </button>
                             </div>
                             <div class="col-md-3">
                                 <button class='like-btn' index="${message.id}">
                                     <i class="bi bi-emoji-heart-eyes" index="${
-                                      message.id
-                                    }"></i>
+										message.id
+									}"></i>
                                     <span class="likeview" id="like_value" index="${
-                                      message.id
-                                    }">${message.favorite_count - 1}</span>
+										message.id
+									}">${message.favorite_count - 1}</span>
                                 </button>
                             </div>
                         </div>
 
                     </div>
         
-                `;
-        }
-    }
+                `
+		}
+	}
 
-    const messagesAllDivs = document.querySelectorAll(".messageAllDivs");
-    for (let messagesDivs of messagesAllDivs) {
-        const editBtn = await messagesDivs.querySelector(".edit-btn");
-        const deleteBtn = await messagesDivs.querySelector(".delete-btn");
-        // const angryBtn = await messagesDivs.querySelector('.angry-btn')
-        const likeBtn = await messagesDivs.querySelector(".like-btn");
-        // console.log(deleteBtn);
+	const messagesAllDivs = document.querySelectorAll('.messageAllDivs')
+	for (let messagesDivs of messagesAllDivs) {
+		const editBtn = await messagesDivs.querySelector('.edit-btn')
+		const deleteBtn = await messagesDivs.querySelector('.delete-btn')
+		// const angryBtn = await messagesDivs.querySelector('.angry-btn')
+		const likeBtn = await messagesDivs.querySelector('.like-btn')
+		// console.log(deleteBtn);
 
-        // Call Edit
-        editBtn.addEventListener("click", async(event) => {
-            const messageEdit = messagesDivs.querySelector(".message-input").value;
-            // console.log(messagesDivs.querySelector('.message-input').disabled);
-            // messagesDivs.querySelector('.message-input').classList.toggle('readonly');
+		// Call Edit
+		editBtn.addEventListener('click', async (event) => {
+			const messageEdit =
+				messagesDivs.querySelector('.message-input').value
+			// console.log(messagesDivs.querySelector('.message-input').disabled);
+			// messagesDivs.querySelector('.message-input').classList.toggle('readonly');
 
-            console.log(messageEdit);
-            const editIndex = event.currentTarget.getAttribute("index");
-            // console.log(editIndex);
-            // Call Edit API
-            const messageEditRes = await fetch("/message/update", {
-                method: "PUT",
-                body: JSON.stringify({
-                    messages_comment: messageEdit,
-                    index: editIndex,
-                }), //記得JSON出寫返headers
-                headers: { "Content-Type": "application/json" },
-            });
-            if (messageEditRes.ok) {
-                loadMessages();
-            }
-        });
+			console.log(messageEdit)
+			const editIndex = event.currentTarget.getAttribute('index')
+			// console.log(editIndex);
+			// Call Edit API
+			const messageEditRes = await fetch('/message/update', {
+				method: 'PUT',
+				body: JSON.stringify({
+					messages_comment: messageEdit,
+					index: editIndex
+				}), //記得JSON出寫返headers
+				headers: { 'Content-Type': 'application/json' }
+			})
+			if (messageEditRes.ok) {
+				loadMessages()
+			}
+		})
 
-        //Call Delete
-        deleteBtn.addEventListener("click", async(event) => {
-            const editIndex = event.currentTarget.getAttribute("index");
-            // console.log(editIndex);
-            // Call Delete API
-            const messageEditRes = await fetch("/message/update", {
-                method: "DELETE",
-                body: JSON.stringify({
-                    index: editIndex,
-                }), //記得JSON出寫返headers
-                headers: { "Content-Type": "application/json" },
-            });
-            if (messageEditRes.ok) {
-                loadMessages();
-            }
-        });
+		//Call Delete
+		deleteBtn.addEventListener('click', async (event) => {
+			const editIndex = event.currentTarget.getAttribute('index')
+			// console.log(editIndex);
+			// Call Delete API
+			const messageEditRes = await fetch('/message/update', {
+				method: 'DELETE',
+				body: JSON.stringify({
+					index: editIndex
+				}), //記得JSON出寫返headers
+				headers: { 'Content-Type': 'application/json' }
+			})
+			if (messageEditRes.ok) {
+				loadMessages()
+			}
+		})
 
-        // let likePattern = true;
-        likeBtn.addEventListener("click", async(event) => {
-            // Call Like API
-            console.log(event.currentTarget.getAttribute("index"));
-            // let likeview = messagesDivs.querySelector('.likeview');
-            // let likeview_num = messagesDivs.querySelector('.likeview').innerHTML
-            // console.log(likePattern)
-            // console.log(likeview);
-            // if (likePattern) {
-            //     console.log(likePattern)
-            //     likeview.innerHTML = parseInt(likeview_num) + 1
-            //     likePattern = false
-            //     console.log(likePattern)
-            //     // return
-            // } else {
-            //     likeview.innerHTML = parseInt(likeview_num) - 1
-            //     likePattern = true
-            //     console.log(likePattern)
-            //     // return
+		// let likePattern = true;
+		likeBtn.addEventListener('click', async (event) => {
+			// Call Like API
+			console.log(event.currentTarget.getAttribute('index'))
+			// let likeview = messagesDivs.querySelector('.likeview');
+			// let likeview_num = messagesDivs.querySelector('.likeview').innerHTML
+			// console.log(likePattern)
+			// console.log(likeview);
+			// if (likePattern) {
+			//     console.log(likePattern)
+			//     likeview.innerHTML = parseInt(likeview_num) + 1
+			//     likePattern = false
+			//     console.log(likePattern)
+			//     // return
+			// } else {
+			//     likeview.innerHTML = parseInt(likeview_num) - 1
+			//     likePattern = true
+			//     console.log(likePattern)
+			//     // return
 
-            const editIndex = event.currentTarget.getAttribute("index");
+			const editIndex = event.currentTarget.getAttribute('index')
 
-            const messageEditRes = await fetch("/message/like", {
-                method: "POST",
-                body: JSON.stringify({
-                    index: editIndex,
-                }), //記得JSON出寫返headers
-                headers: { "Content-Type": "application/json" },
-            });
-            if (messageEditRes.ok) {
-                loadMessages();
-            }
-        });
-    }
+			const messageEditRes = await fetch('/message/like', {
+				method: 'POST',
+				body: JSON.stringify({
+					index: editIndex
+				}), //記得JSON出寫返headers
+				headers: { 'Content-Type': 'application/json' }
+			})
+			if (messageEditRes.ok) {
+				loadMessages()
+			}
+		})
+	}
 }
 
-loadMessages();
+loadMessages()
