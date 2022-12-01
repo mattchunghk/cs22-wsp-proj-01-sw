a928f34647a5dc5e261870f8380e481d777532e9
mattchunghk <mattchunghk@gmail.com>
2022-12-01 17:17:38 +0800

    git

---
+++ public/loginPage/login 2.js
@@ -0,0 +1,53 @@
+// import { io } from '../app'
+console.log('js running')
+window.onload = () => {
+	init()
+}
+
+function init() {
+	document.querySelector('.login-form').addEventListener('submit', login)
+	document
+		.querySelector('.back-home-form')
+		.addEventListener('click', backHome)
+	document.querySelector('#register-btn').addEventListener('click', goSignUp)
+	document
+		.querySelector('.logo-container')
+		.addEventListener('click', backHome)
+}
+
+async function login(event) {
+	event.preventDefault()
+
+	// Serialize the Form afterwards
+	const form = event.target
+	const formObject = {}
+	const loginText = document.querySelector('.login-text')
+	formObject['username'] = form.username.value
+	formObject['password'] = form.password.value
+	const res = await fetch('/user/login', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(formObject)
+	})
+
+	if (res.ok) {
+		loginText.innerHTML = 'Login successful!'
+		setTimeout(() => (window.location = document.referrer), 1000)
+		loginText.style.fontSize = '1.3rem'
+	} else {
+		loginText.innerHTML = 'Invalid username or password'
+		loginText.style.fontSize = '1.3rem'
+		setTimeout(() => (loginText.innerHTML = 'Please Login'), 1000)
+		setTimeout(() => (loginText.style.fontSize = '1.3rem'), 1000)
+	}
+}
+
+function goSignUp() {
+	window.location.href = '../register/register.html'
+}
+
+function backHome() {
+	window.location.href = '/'
+}
---
+++ public/register/register 2.js
@@ -0,0 +1,49 @@
+// import { io } from '../app'
+console.log('js running')
+window.onload = () => {
+	init()
+}
+
+function init() {
+	document.querySelector('.login-form').addEventListener('submit', register)
+	document
+		.querySelector('.back-home-form')
+		.addEventListener('click', backHome)
+	document
+		.querySelector('.logo-container')
+		.addEventListener('click', backHome)
+}
+async function register(event) {
+	event.preventDefault()
+
+	// Serialize the Form afterwards
+	const form = event.target
+
+	const formObject = {}
+	const loginText = document.querySelector('.login-text')
+	formObject['username'] = form.username.value
+	formObject['password'] = form.password.value
+	formObject['checkPassword'] = form.checkPassword.value
+	const res = await fetch('/user/register', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(formObject)
+	})
+
+	if (res.ok) {
+		loginText.innerHTML = 'Sign-up successful!'
+		setTimeout(() => (window.location.href = '/'), 1000)
+		loginText.style.fontSize = '1.3rem'
+	} else {
+		loginText.innerHTML = 'Invalid username or password'
+		loginText.style.fontSize = '1.3rem'
+		setTimeout(() => (loginText.style.fontSize = '1.5rem'), 1000)
+		setTimeout(() => (loginText.innerHTML = 'Sign Up'), 1000)
+	}
+}
+
+function backHome() {
+	window.location.href = '/'
+}
